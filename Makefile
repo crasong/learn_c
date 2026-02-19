4abd07831a8925b8632fbabd0d9eff1d36342805
crasong <crasonghwang@gmail.com>
2026-02-19 22:46:55 +0800

    Rename Image class, prepare for RayLib, add compile_commands.json

---
+++ Makefile
@@ -0,0 +1,8 @@
+CC = gcc
+CFLAGS = -Wall -Wextra -Ithird_party
+
+learn_c: main.c
+	$(CC) $(CFLAGS) main.c -o learn_c
+
+clean:
+	rm -f learn_c
--- main.c
+++ main.c
@@ -1,71 +1,63 @@
+#include "raylib.h"
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "stb_image_write.h"
-#include <stdio.h>
 #include <stdlib.h>
 
 typedef struct {
-    int w, h;
-    unsigned char *pixels;
-} Image;
+  int w, h;
+  unsigned char *pixels;
+} MyImage;
 
 typedef struct {
-    int x, y;
-    int w, h;
-    int color[3];
+  int x, y;
+  int w, h;
+  int color[3];
 } Square;
 
-void SetColorForPixel(Image* img, int x, int y, int rgb[3]) {
-    
-    img->pixels[(y * img->w + x) * 3 + 0] = rgb[0];
-    img->pixels[(y * img->w + x) * 3 + 1] = rgb[1];
-    img->pixels[(y * img->w + x) * 3 + 2] = rgb[2];
+void SetColorForPixel(MyImage *img, int x, int y, int rgb[3]) {
+
+  img->pixels[(y * img->w + x) * 3 + 0] = rgb[0];
+  img->pixels[(y * img->w + x) * 3 + 1] = rgb[1];
+  img->pixels[(y * img->w + x) * 3 + 2] = rgb[2];
 }
 
-void FillGradient(Image* img) {
-    int color[3] = {0, 0, 128};
-    for (int y = 0; y < img->h; ++y) {
-        for (int x = 0; x < img->w; ++x) {
-            color[0] = x;
-            color[1] = y;
-            SetColorForPixel(img, x, y, color);
-        }
+void FillGradient(MyImage *img) {
+  int color[3] = {0, 0, 128};
+  for (int y = 0; y < img->h; ++y) {
+    for (int x = 0; x < img->w; ++x) {
+      color[0] = x;
+      color[1] = y;
+      SetColorForPixel(img, x, y, color);
     }
+  }
 }
 
-void DrawSquare(Image* img, Square* params) {
-    int lastX = params->x + params->w;
-    int lastY = params->y + params->h;
-    for (int y = params->y; y < lastY; ++y) {
-        for (int x = params->x; x < lastX; ++x) {
-            SetColorForPixel(img, x, y, params->color);
-        } 
+void DrawSquare(MyImage *img, Square *params) {
+  int lastX = params->x + params->w;
+  int lastY = params->y + params->h;
+  for (int y = params->y; y < lastY; ++y) {
+    for (int x = params->x; x < lastX; ++x) {
+      SetColorForPixel(img, x, y, params->color);
     }
+  }
 }
 
-int main(int argc, char** argv) {
-    if (argc < 3) return 1; // need 2 args to proceed
-    
-    int w = atoi(argv[1]);
-    int h = atoi(argv[2]);
-    Image img = {
-        .w = w,
-        .h = h,
-        .pixels = malloc(w * h * 3)
-    };
+int main(int argc, char **argv) {
+  if (argc < 3)
+    return 1; // need 2 args to proceed
 
-    Square square = {
-        .x = w/2,
-        .y = h/2,
-        .w = w/4,
-        .h = h/4,
-        .color = { 128, 45, 22 }
-    };
-    
-    FillGradient(&img);
-    DrawSquare(&img, &square);
-    
-    stbi_write_png("out.png", w, h, 3, img.pixels, w * 3);
-    
-    free(img.pixels);
-    return 0;
+  int w = atoi(argv[1]);
+  int h = atoi(argv[2]);
+  MyImage img = {.w = w, .h = h, .pixels = malloc(w * h * 3)};
+
+  Square square = {
+      .x = w / 2, .y = h / 2, .w = w / 4, .h = h / 4, .color = {128, 45, 22}};
+
+  FillGradient(&img);
+  DrawSquare(&img, &square);
+
+  stbi_write_png("out.png", w, h, 3, img.pixels, w * 3);
+
+  free(img.pixels);
+  return 0;
 }
